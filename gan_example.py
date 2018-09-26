faeb51a855d079e7690917e4cf96ac32462f771e
Tom <32938439+TTitcombe@users.noreply.github.com>
2018-09-26 14:55:42 +0100

    Update gan_example.py

--- gan_example.py
+++ gan_example.py
@@ -4,7 +4,11 @@ import tensorflow as tf
 from Models.gan_model import GANModel
 from Training.gan_trainer import GANTrainer
 
-IMAGE_PATH = './celeba/' #DIRECTORY TO IMAGES TO TRAIN ON
+IMAGE_PATH = './celeba/' #DIRECTORY TO IMAGES TO TRAIN ON. 
+BATCH_SIZE = 256
+N_EPOCHS = 100
+PRINT_EVERY = 1
+
 x_dim = 178 * 218 * 3 #because I'm using celeba
 z_dim = 100
 
@@ -12,4 +16,4 @@ model = GANModel(x_dim, z_dim)
 trainer = GANTrainer(model)
 
 print("Training a GAN..")
-trainer.train(IMAGE_PATH, 256, 1000, 1)
+trainer.train(IMAGE_PATH, BATCH_SIZE, N_EPOCHS, PRINT_EVERY)
